b9ee677d2d4fabfa24aa1673b128775dd10733db
henryhannn <henryhan3996@gmail.com>
2020-06-21 20:18:27 -0700

    added bfs

--- w4d1/graphnodes.rb
+++ w4d1/graphnodes.rb
@@ -7,6 +7,19 @@ class GraphNode
         @val = val
         @neighbors = []
     end
+end
 
-    
+def bfs(starting_node, target_value)
+    queue = [starting_node]
+    visited_node = Set.new()
+
+    while !queue.empty?
+        node = queue.shift
+        if !visited.include?(node)
+            return node.val if node.val == target_value
+            visited.add(node)
+            queue += node.neighbors
+        end
+    end
+    nil
 end
