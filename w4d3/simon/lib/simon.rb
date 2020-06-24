46f03b54e4bc357ed07a1b0b5798518a18043f7d
henryhannn <henryhan3996@gmail.com>
2020-06-23 22:55:31 -0700

    updated require_sequence and made the game playable

--- w4d3/simon/lib/simon.rb
+++ w4d3/simon/lib/simon.rb
@@ -18,21 +18,37 @@ class Simon
   end
 
   def take_turn
+    p "Get ready..."
+    sleep(2)
+    system("clear")
     show_sequence
     require_sequence
     case game_over
     when false
       round_success_message
       @sequence_length += 1
+      system("clear")
     end
   end
 
   def show_sequence
     add_random_color
+    @seq.each do |color|
+      p color
+      sleep(1)
+      system("clear")
+    end
   end
 
   def require_sequence
-    
+    p "Please enter the sequence"
+    @seq.each do |color|
+      answer = gets.chomp
+      if answer != color
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
@@ -40,11 +56,11 @@ class Simon
   end
 
   def round_success_message
-
+    p "You got it all right!!"
   end
 
   def game_over_message
-
+    p "You answered incorrectly. Game Over."
   end
 
   def reset_game
@@ -53,3 +69,7 @@ class Simon
     @seq = []
   end
 end
+
+
+x = Simon.new
+x.play
