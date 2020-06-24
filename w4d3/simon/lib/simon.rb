6c2bb791c05e77be08851a99975379c8c2430610
henryhannn <henryhan3996@gmail.com>
2020-06-23 22:37:41 -0700

    added base methods

--- w4d3/simon/lib/simon.rb
+++ w4d3/simon/lib/simon.rb
@@ -4,27 +4,39 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until game_over == true
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    case game_over
+    when false
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
-
+    
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
@@ -36,6 +48,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
