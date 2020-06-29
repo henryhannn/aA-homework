85c2def5a5c6e27a794a4b568172692850f09b58
henryhannn <henryhan3996@gmail.com>
2020-06-29 08:43:58 -0700

    updated methods

--- w5d1/LRU_cache.rb
+++ w5d1/LRU_cache.rb
@@ -1,20 +1,27 @@
 class LRUCache
-    def initialize
+    def initialize(size)
+      @size = size
     end
 
     def count
-
+      @cache.count
     end
 
     def add(el)
-
+      if @cache.include?(el)
+        @cache.delete(el)
+        @cache << el
+      elsif count >= @size
+        @cache.shift
+        @cache << el
+      else
+        @cache << el
+      end
     end
 
     def show
-
+      p @cache
+      nil
     end
-
-    private
-
 
   end
