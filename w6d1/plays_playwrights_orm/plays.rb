44162a9f307268a10c219421a0f6bab78476c24c
henryhannn <henryhan3996@gmail.com>
2020-07-06 00:20:50 -0700

    updated Play class

--- w6d1/plays_playwrights_orm/plays.rb
+++ w6d1/plays_playwrights_orm/plays.rb
@@ -19,6 +19,36 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+    
+    return nil unless play.length > 0
+    Play.new(play.first)
+  end
+
+  def self.find_by_playwright(name)
+    playwright = Playwright.find_by_name(name)
+    raise "#{name} not found in DB" unless playwright
+
+    plays = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+
+    plays.map { |play| Play.new(play) }
+  end
+  
   def initialize(options)
     @id = options['id']
     @title = options['title']
